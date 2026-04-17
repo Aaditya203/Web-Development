42aa736b768a768110f4b53c056a91e20d83cff4
Aditya Sharma <adityas20032005@gmail.com>
2026-04-17 21:09:32 +0530

    Change MongoDB connection string to placeholder
    Updated MongoDB connection string to use a placeholder.

--- day12/MongoDB/db/index.js
+++ day12/MongoDB/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://admin:7EdELII0KXE8LzbL@cluster0.929ebve.mongodb.net/course_selling_app');
+mongoose.connect('your-url');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
