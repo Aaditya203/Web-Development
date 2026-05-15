f5d97173a70003229781319f3ff3603d5271323c
Aditya Sharma <adityas20032005@gmail.com>
2026-05-15 20:38:31 +0530

    useEffect hook

--- day14/practice/src/App.jsx
+++ day14/practice/src/App.jsx
@@ -1,35 +1,104 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import './App.css'
 
-function App() {
-  return (
-    <div>
-    <HeaderButton/>
-    <Header title="Hi there"/>
-    <Header title="Hi there"/>
-    <Header title="Hi there"/>
-    <Header title="Hi there"/>
-    </div>
-  )
-}
+import { useEffect, useState } from "react"
 
-function HeaderButton(){
-  const [title,setTitle] = useState("My name is Aditya");
-  function change(){
-    setTitle("My name is "+Math.random())
-  }
+// function App(){
+//   let count = 4
+//   const [todos,setTodos] = useState([{
+//     id:3,
+//     title:'Do GYM',
+//     description:"Go to gym at 6 in morning"
+//   },{
+//     id:1,
+//     title:'Solve DSA',
+//     description:"Do DSA at 10 in morning"
+//   },{
+//     id:2,
+//     title:'Do web dev',
+//     description:"Do web dev and create some projects"
+//   }])
+//   function addTodo(){
+//     setTodos([...todos,{
+//       id:count++,
+//       title:Math.random(),
+//       description:Math.random()
+      
+//     }])
+//   }
+//   return (
+//     <div>
+//       <button onClick={addTodo}>Add a Todo</button>
+//       {todos.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description}/>)}
+//     </div>
+//   )
+// }
+// function Todo({title,description}){
+//   return <div>
+//     <h1>{title}</h1>
+//     <h3>{description}</h3>
+//   </div>
+// }
+
+
+// function App(){
+//   return <div>
+//     <CardWrapper>
+//       <CardWrapper>
+//       Hi there
+//       </CardWrapper>
+//     </CardWrapper>
+//     <CardWrapper>
+//       Hello How are you!
+//     </CardWrapper>
+//   </div>
+// }
+
+// function CardWrapper({children}){
+//   return <div style={{border: '2px solid black',padding:20,marginBottom:10}}>
+//     {children}
+//   </div>
+// }
+// function App(){
+//   useEffect(()=>{
+//     alert("Hi")
+//   },[]);
+//   return <div>
+//     hi there
+//   </div>
+// }
+
+function App(){
+  const [todos,setTodos] = useState([])
+
+  useEffect(()=>{
+    const fetchTodos = async ()=>{
+      const res = await fetch("http://localhost:3000/todos")
+      const jsoN = await res.json();
+      const data = jsoN.todos
+      setTodos(data)
+
+    }
+    fetchTodos()
+    const interval = setInterval(()=>{
+      fetchTodos()
+    },10000)
+
+    return () => clearInterval(interval)
+  },[])
+
   return <div>
-    <button onClick={change}>Click me!</button>
-    <Header title={ title }/>
+    {todos.map(todo=> <Todo key={todo.id} title={todo.title} description={todo.description}/> )}
   </div>
 }
 
-function Header(props){
+function Todo({title,description}){
   return <div>
-    <h1>{props.title}</h1>
+    <h1>{title}</h1>
+    <h3>{description}</h3>
   </div>
 }
-export default App
+export default App;
